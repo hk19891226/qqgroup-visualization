050f264ec492577b151237ac048c60f704f0b81a
鸡毛 <shihao.gu@perfma.com>
2018-10-03 13:15:02 +0800

    提交主要代码文件

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="/qqimg?dst_uin=1982775886&spec=640">
+    <img src="/groupimg/419056682/419056682/640" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
