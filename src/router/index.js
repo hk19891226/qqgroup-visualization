d54ab51d0cd89ab51e0560bfb95cd30d3a3a6b13
鸡毛 <shihao.gu@perfma.com>
2018-10-04 15:01:02 +0800

    接口对接

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import viewQQTable from "@/views/qqTable";
 import viewGroupTable from "@/views/groupTable";
 import viewGraph2d from "@/views/graph2d";
+import viewGraph3d from "@/views/graph3d";
 
 Vue.use(Router);
 
@@ -27,6 +28,11 @@ export default new Router({
             path: "/2d",
             name: "viewGraph2d",
             component: viewGraph2d,
+        },
+        {
+            path: "/3d",
+            name: "viewGraph3d",
+            component: viewGraph3d,
         },
     ]
 })
---
+++ src/views/graph3d.vue
@@ -0,0 +1,117 @@
+<!--局部样式-->
+<style scoped>
+    .viewGraph3d {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        padding-top: 60px;
+    }
+</style>
+
+<!--局部覆盖样式-->
+<style>
+</style>
+
+<template>
+    <div class="viewGraph3d">
+        <div class="graph"></div>
+    </div>
+</template>
+
+<script>
+    import api from "@/api";
+    import ForceGraph3D from "3d-force-graph";
+
+    export default {
+        name: "viewGraph3d",
+        props: {
+
+        },
+        data () {
+            return {
+                //#region 页面对象
+                    graph: null,
+                //#endregion
+
+                //#region 页面内容绑定数据
+                    searchType: "qq",
+                    searchNum: "",
+                //#endregion
+
+                //#region 页面样式绑定数据
+                //#endregion
+            };
+        },
+        watch: {
+
+        },
+        computed: {
+            //#region 常量计算属性
+            //#endregion
+
+            //#region 数据转换计算属性
+            //#endregion
+            
+            //#region 样式计算属性
+            //#endregion
+        },
+        methods: {
+            //#region 页面事件方法
+            //#endregion
+
+            //#region 业务逻辑方法
+                async b_updateGraph () {
+                    let result = null;
+                    if (this.searchType == "qq") {
+                        result = await api.queryQQExtGraph(this.searchNum);
+                    }
+                    else if (this.searchType == "group") {
+                        result = await api.queryGroupGraph(this.searchNum);
+                    }
+                    if (result) {
+                        let groupList = result.group.map(item => ({ id: item.groupNum, group: item }));
+                        let memberList = result.member.map(item => ({ id: item.memberQQNum, member: item }));
+                        let linkList = result.link.map(item => ({ source: item.linkQQNum, target: item.linkGroupNum, link: item }));
+                        let gData = {
+                            nodes: groupList.concat(memberList),
+                            links: linkList,
+                        };
+                        this.graph.graphData(gData);
+                    }
+                },
+            //#endregion
+
+            //#region 接口访问方法
+            //#endregion
+
+            //#region 数据转换方法
+            //#endregion
+
+            //#region 自动样式方法
+            //#endregion
+
+            //#region 其他方法
+            //#endregion
+        },
+        created () {
+            this.searchType = this.$route.query.search;
+            if (!this.searchType) {
+                this.searchType = "qq";
+            }
+            this.searchNum = this.$route.query.num;
+            if (!this.searchNum) {
+                this.searchNum = "10001";
+            }
+            this.b_updateGraph();
+        },
+        mounted () {
+            const forceGraph3D = ForceGraph3D();
+            this.graph = forceGraph3D(this.$el.querySelector(".graph"));
+        },
+        components: {
+
+        },
+    };
+</script>
