2cd0c0ca89922f51aaf1e7c748d0b5e30a494c6c
鸡毛 <shihao.gu@perfma.com>
2018-10-07 13:48:17 +0800

    球体小窗

--- src/views/graph3d.vue
+++ src/views/graph3d.vue
@@ -8,14 +8,78 @@
         height: 100%;
         padding-top: 60px;
     }
+
+
 </style>
 
 <!--局部覆盖样式-->
 <style>
+    .qqWindow {
+        display: inline-flex;
+        align-items: center;
+        box-sizing: border-box;
+        padding: 10px;
+        background-color: rgba(51, 51, 51, 0.7);
+        border-radius: 4px;
+        font-size: 12px;
+        color: #ddd;
+    }
+
+    .qqWindow > img {
+        width: 100px;
+        height: 100px;
+        border: solid 1px #999;
+    }
+
+    .qqWindow > .infoWarp {
+        width: 150px;
+        height: 100px;
+        margin-left: 10px;
+    }
+
+    .qqWindow > .infoWarp label {
+        color: white;
+        font-weight: bold;
+    }
+
+    .qqWindow > .infoWarp > div {
+        height: 20px;
+    }
+
+    .qqWindow > .infoWarp > ul {
+        box-sizing: border-box;
+        list-style-type: none;
+        margin: 0px;
+        padding: 0px;
+        height: 60px;
+        overflow-y: auto;
+        border: solid 1px #999;
+    }
 </style>
 
 <template>
     <div class="viewGraph3d">
+        <!-- <div class="qqWindow">
+            <img :src="qqImgUrl('1982775886')" />
+            <div class="infoWarp">
+                <div>
+                    <label>QQ:</label>
+                    <span>1982775886</span>
+                </div>
+                <div>
+                    <label>群内昵称：</label>
+                </div>
+                <ul>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                    <li>你好，世界</li>
+                </ul>
+            </div>
+        </div> -->
         <div class="graph"></div>
     </div>
 </template>
@@ -72,59 +136,131 @@
                         result = await api.queryGroupGraph(this.searchNum);
                     }
                     if (result) {
-                        console.log(result);
-                        let groupList = result.group.map(item => ({
-                            id: item.groupNum,
-                            type: "group",
-                            name: item.groupTitle,
-                            group: item,
-                        }));
-                        let memberList = result.member.map(item => ({
-                            id: item.memberQQNum,
-                            type: "member",
-                            name: item.memberQQNum,
-                            member: item,
-                        }));
-                        let linkList = result.link.map(item => ({
-                            source: item.linkQQNum,
-                            target: item.linkGroupNum,
-                            name: item.linkNick,
-                            link: item,
-                        }));
-                        let gData = {
-                            nodes: groupList.concat(memberList),
-                            links: linkList,
-                        };
-                        this.graph.graphData(gData)
-                        .nodeAutoColorBy('id')
-                        .nodeThreeObject(node => {
-                            let text = "";
-                            if (node.type == "group") {
-                                text = node.group.groupTitle;
+                        let data = this.graphData(result);
+                        this.graph
+                        .nodeId("nodeId")
+                        .linkSource("sourceId")
+                        .linkTarget("targetId")
+                        .nodeAutoColorBy("nodeId")
+                        // .nodeThreeObject(node => {
+                        //     const sprite = new SpriteText(node.nodeLabel);
+                        //     sprite.color = node.color;
+                        //     sprite.textHeight = 8;
+                        //     return sprite;
+                        // })
+                        .nodeLabel(node => {
+                            let imgUrl = "";
+                            if (node.nodeType == "group") {
+                                imgUrl = this.groupImgUrl(node.nodeId);
                             }
-                            else if (node.type == "member") {
-                                let link = linkList.find(link => link.source == node.member.memberQQNum);
-                                if (link) {
-                                    text = link.link.linkNick;
-                                }
-                                else {
-                                    text = node.member.memberQQNum;
-                                }
+                            else if (node.nodeType == "member") {
+                                imgUrl = this.qqImgUrl(node.nodeId);
                             }
-                            const sprite = new SpriteText(text);
-                            sprite.color = node.color;
-                            // sprite.color = node.color;
-                            sprite.textHeight = 8;
-                            return sprite;
-                        });
+                            return this.qqWindowHtml(node);
+                        })
+                        .graphData(data);
                     }
                 },
             //#endregion
 
-            //#region 接口访问方法
+            //#region 资源访问方法
+                //传入图片Url获取图片资源，返回一个Promise对象
+                getImg (url, timeout = 60000) {
+                    return new Promise((resolve, reject) => {
+                        try {
+                            let imgObj = new Image();
+                            imgObj.src = url;
+                            let id = setTimeout(() => {
+                                reject();
+                            }, timeout);
+                            imgObj.onload = () => {
+                                clearTimeout(id);
+                                resolve(imgObj);
+                            };
+                            imgObj.onerror = () => {
+                                reject();
+                            };
+                        }
+                        catch (e) {
+                            reject();
+                        }
+                    });
+                },
             //#endregion
 
             //#region 数据转换方法
+                //获取QQ头像的Url地址
+                qqImgUrl (qqNum) {
+                    return `/qqimg?dst_uin=${ qqNum }&spec=100`;
+                },
+                //获取群头像的Url地址
+                groupImgUrl (groupNum) {
+                    return `/groupimg/${ groupNum }/${ groupNum }/100`;
+                },
+
+                //根据接口返回数据格式整理出力导向图实际可用的数据格式
+                graphData (data) {
+                    let linkList = data.link.map(link => {
+                        return {
+                            sourceId: link.linkQQNum,
+                            targetId: link.linkGroupNum,
+                            linkLabel: link.linkNick,
+                            linkValue: link,
+                        };
+                    });
+                    let groupList = data.group.map(group => {
+                        return {
+                            nodeId: group.groupNum,
+                            nodeType: "group",
+                            nodeLabel: group.groupTitle,
+                            nodeValue: group,
+                        };
+                    });
+                    let memberList = data.member.map(member => {
+                        return {
+                            nodeId: member.memberQQNum,
+                            nodeType: "member",
+                            nodeLabel: "",
+                            nodeValue: member,
+                        };
+                    });
+                    //回查群成员节点群昵称信息
+                    memberList.forEach(member => {
+                        let link = linkList.find(link => link.sourceId == member.nodeId);
+                        if (link) {
+                            member.nodeLabel = link.linkLabel;
+                        }
+                        else {
+                            member.nodeLabel = "未知QQ昵称";
+                        }
+                    });
+                    let nodeList = groupList.concat(memberList);
+                    return {
+                        nodes: nodeList,
+                        links: linkList,
+                    };
+                },
+
+                qqWindowHtml (node) {
+                    let temp = `
+                        <div class="qqWindow">
+                            <img src="${ this.qqImgUrl(node.nodeId) }" />
+                            <div class="infoWarp">
+                                <div>
+                                    <label>QQ:</label>
+                                    <span>${ node.nodeId }</span>
+                                </div>
+                                <div>
+                                    <label>群内昵称：</label>
+                                </div>
+                                <ul>
+                                    <li>${ node.nodeLabel }</li>
+                                </ul>
+                            </div>
+                        </div>
+                    `;
+                    return temp;
+                },
             //#endregion
 
             //#region 自动样式方法
